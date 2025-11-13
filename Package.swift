38682a8ba03de149ad0f3d2d55718939b418163e
Bitrise CI <noreply@bitrise.io>
2025-11-13 05:59:41 +0000

    Update 0.0.79

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "Common",
             url: "https://github.com/Truvideo/truvideo-sdk-ios-common/releases/download/0.0.79/shared.xcframework.zip",
-            checksum: ""
+            checksum: "eeeef299ab3119c8e813a5ec57ac9c349bb3c51827ed21279da84ba7913fd9e4"
         ),
         .target(
             name: "CommonTargets",
